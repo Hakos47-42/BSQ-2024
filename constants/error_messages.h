5613ac4e91a3b2d3888efd69fd149770f32efc20
Enrique Santana Mesa <esantana@c1r4s5.42malaga.com>
2024-07-08 19:47:56 +0200

    utils + print error

---
+++ constants/error_messages.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_messages.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esantana <esantana@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 19:40:39 by esantana          #+#    #+#             */
+/*   Updated: 2024/07/08 19:41:15 by esantana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// error_messages.h
+
+#ifndef ERROR_MESSAGES_H
+# define ERROR_MESSAGES_H
+
+// Define your error messages here
+# define ERR_FILE_NOT_FOUND "Error: File not found.\n"
+# define ERR_INVALID_ARGS "Error: Invalid arguments.\n"
+# define ERR_OUT_OF_MEMORY "Error: Out of memory.\n"
+# define ERR_PERMISSION_DENIED "Error: Permission denied.\n"
+
+#endif // ERROR_MESSAGES_H
