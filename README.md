06e666e210ca2313c140b5ce5b3b889d3c8a7451
Enrique Santana Mesa <esantana@c1r3s4.42malaga.com>
2024-07-08 22:22:00 +0200

    read 1st line from buffer and validate it

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Repositorio para centralizar los esfuerzos del proyecto BSQ de la piscina de junio 2024 de 42 Málaga 🏊.
+# Repositorio Proyecto BSQ - 42 Málaga 🏊
+
+## Recomendaciones
+
+- **Comentar Todo el código**: Especialmente las cabeceras de las funciones. Esto permite que, al poner el ratón encima desde VSCode, se vea una breve descripción.
+- **Usar anotaciones**: Como `TODO` y `FIXME`, para detectar tareas pendientes y errores de código fácilmente.
+- **Buenas prácticas**: Por favor, evitemos el código espagueti para mantener la claridad en el proyecto, organización por carpetas.
+
--- utils/ft_error_handler.c
+++ utils/ft_error_handler.c
@@ -6,12 +6,13 @@
 /*   By: esantana <esantana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 19:35:58 by esantana          #+#    #+#             */
-/*   Updated: 2024/07/08 19:48:56 by esantana         ###   ########.fr       */
+/*   Updated: 2024/07/08 22:09:02 by esantana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+/**Prints an array of chars */
 void	print_str(const char *str)
 {
 	while (*str)
