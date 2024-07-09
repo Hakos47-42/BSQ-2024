4b8f0f1a3e98ca0ca546a79fadb1bcf1d10fe7cc
iguizan- <iguizan-@c1r2s3.42malaga.com>
2024-07-09 19:54:27 +0200

    functional version

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 //char *ft_process_file(char *file_name);
 char **validate_map(char *file_name);
 void clean_map(char **map);
-void ft_print_map(char **map);
+void ft_print_map(char **map, int *y_x);
 // square matrix_iterator(**char matrix, char *simbols, int *size_yx)
 /*
     square = {
@@ -17,6 +17,7 @@ void ft_print_map(char **map);
 int main(int argc, char *argv[])
 {
     char **map;
+    int y_x[] = {9, 27};
 
     if (argc != 2)
     {
@@ -24,7 +25,7 @@ int main(int argc, char *argv[])
         exit(1);
     }
     map = validate_map(argv[1]);
-    ft_print_map(map);
+    ft_print_map(map, y_x);
     free(map);
     return (0);
 }
