259887f60271f377d2817408ea7051898f192d0c
iguizan- <iguizan-@c1r2s3.42malaga.com>
2024-07-09 15:16:37 +0200

    map creator

--- read/map.c
+++ read/map.c
@@ -1,6 +1,32 @@
 #include <stdlib.h>
 
-char **create_map(char *buffer , int *args_x_y)
+char **asigned_map(char **map, char *buffer)
+{
+    int i;
+    int lines;
+    int col;
+    int init;
+
+    init = 1;
+    i = 0;
+    lines = 0;
+    col = 0;
+    while (buffer[i])
+    {
+        if (buffer[i] != '\n' && init != 1)
+            map[lines][col] = buffer[i];
+        else if (buffer[i] == '\n')
+        {
+            init = 0;
+            col = 0;
+            lines++;
+        }
+        i++;
+    }
+    return (map);
+}
+
+char **create_map(char *buffer, int *args_x_y)
 {
     char **map;
     int lines;
@@ -15,26 +41,4 @@ char **create_map(char *buffer , int *args_x_y)
         map[i] = malloc(args_x_y[0] * sizeof(int));
     }
     return (asigned_map(map, buffer));
-}
-
-char **asigned_map(char **map, char *buffer)
-{
-    int i;
-    int lines;
-    int col;
-    i = 0;
-    lines = 0;
-    col = 0;
-    while (buffer[i])
-    {
-        if (buffer[i] != '\n')
-            map[lines][col] = buffer[i];
-        else if (buffer[i] == '\n')
-            {
-                col = 0;
-                lines++;
-            }
-        i++;
-    }
-    return (map);
 }
