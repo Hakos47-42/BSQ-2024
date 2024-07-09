0b245659aea04469e2da20b157f492e08aa708b1
iguizan- <iguizan-@c1r2s3.42malaga.com>
2024-07-09 12:42:57 +0200

    fix: main

---
+++ read/validate_map.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char	*ft_loadmap(int fd);
+char	*ft_process_file(char *file_name);
+int valid_buffer(char *buffer)
+{
+    (void) buffer;
+    return (1);
+}
+
+char *validate_map(char *file_name)
+{
+    char *buffer;
+    buffer = ft_process_file(file_name);
+
+    if (!valid_buffer(buffer))
+    {
+        printf("map error\n");
+        exit(1);
+    }
+    return (buffer);
+}
