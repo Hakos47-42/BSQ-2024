01a74f34c6c571b330b70e5e040a8d57f63c171a
Enrique Santana Mesa <esantana@c1r2s2.42malaga.com>
2024-07-10 18:12:32 +0200

    letsgo

--- file_options.h
+++ file_options.h
@@ -6,19 +6,18 @@
 /*   By: esantana <esantana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 16:53:11 by pabmart2          #+#    #+#             */
-/*   Updated: 2024/07/10 16:34:12 by esantana         ###   ########.fr       */
+/*   Updated: 2024/07/10 16:54:15 by esantana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILE_OPTIONS_H
 # define FILE_OPTIONS_H
 
-struct							s_file_options
+typedef struct							s_file_options
 {
 	int							y_x[2];
 	char						*chars;
 	char						**map;
-};
-typedef struct s_file_options	t_file_options;
+} t_file_options;
 
 #endif
